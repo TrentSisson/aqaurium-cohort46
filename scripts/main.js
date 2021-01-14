f64f4b71ffc522c0a700f27e665b5e1851a1bd34
TrentSisson <trent.c.sisson0@gmail.com>
2021-01-14 10:48:40 -0600

    completed tiplist and rendered tips to the dom

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import {useFish} from "./fish/FishDataProvider.js"
 import {FishList} from "./fish/fishList.js"
+import {TipList} from "./tips/TipList.js"
 
 FishList()
+TipList()
 
 const allTheFish = useFish()
 for (const fish of allTheFish) {
